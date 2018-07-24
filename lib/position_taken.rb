8d8b0454cede9784d392ad55d25567585bd23c42
Pamela Bortnick <pbortnick@gmail.com>
2018-07-24 23:07:08 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,33 +1,9 @@
 def position_taken?(board, index)
-  if board[index] == "" || board[index] == " " || board[index] == nil
-    return false
+  if board[index] ==  " " || board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def position_taken?(board, index)
-#   if board[index] ==  " " || board[index] == ""
-#     false
-#   elsif board[index] == nil
-#     false
-#   elsif board[index] == "X" || board[index] == "O"
-#     true
-#   end
-# end
